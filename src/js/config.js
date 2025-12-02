e0719294ea70bfb044a2e9fc3e2485ed3e237287
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-12-02 16:31:15 +0300

    uploading a new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,7 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
+
+export const KEY = "70ae4193-7abd-443e-855c-e06cb8180c05";
+
+export const MODAL_CLOSE_SEC = 2.5;
