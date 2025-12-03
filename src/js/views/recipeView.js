c90ef5e26989ebca4c13e663ecf32780ca10ef19
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-12-03 15:30:16 +0300

    fraction fix

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,8 @@
 import View from "./View.js";
 
 import icons from "url:../../img/icons.svg";
-const Fraction = require("fraction.js");
+// const Fraction = require("fraction.js");
+import Fraction from "fraction.js";
 
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
